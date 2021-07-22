71db61f2a488a3d2943dd07252e031ca64db8da3
seleb1 <bgm1425@gmail.com>
2021-07-22 07:22:35 +0000

    ver.0.0.1

--- Func.py
+++ Func.py
@@ -1,30 +1,54 @@
 import numpy as np
-
-## Copyright 2021 Signal Processing and Machine Learning at Inha Univ.
-## arthor: Gyeongmin Bae
+import weakref
+## Copyright 2021. (이름) All pictures cannot be copied without permission.
 ## date : 2021 - 07 -21
 
 from Var import Variable
 
 # input : instance of Variable
 class Function:
-    def __call__(self, input): 
-        x  = input.data
-        y = self.forward(x)
-        output  = Variable(y)
-        output.set_creator(self) # save creator
-        self.input = input # save input
-        self.output = output # save output
+    def __call__(self, *inputs): 
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
 
-        return output
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+
+        outputs = [Variable(as_array(y)) for y in ys]
+        
+        self.generation = max([x.generation for x in inputs])
+        for output in outputs :
+            output.set_creator(self)
+
+        self.inputs = inputs # save input
+        self.outputs =[weakref.ref(output) for output in outputs] # save output
+
+        return outputs if len(outputs) > 1 else outputs[0]
 
     # i'd not define definition of forward function. 
     # Try to define it through overriding.
-    def forward(self, x):
+    def forward(self, xs):
         raise NotImplementedError()
 
-    def backward(self, gy):
+    def backward(self, gys):
         raise NotImplementedError()
+
+def as_array(x):
+    if np.isscalar(x) :
+        return np.array(x)
+    return x
+
+# Add function
+# number of inputs of Add function is 2 (tuple or list)
+#
+class Add(Function):
+    def forward(self, x0,x1):
+        y = x0 +  x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+
 
 # yeah, i define square function.
 # you can do it too.
@@ -34,14 +58,44 @@ class Square(Function):
         return x**2
     
     def backward(self, gy):
-        x = self.input.data
+        x = self.inputs[0].data
         gx = 2*x*gy
         return gx
 
-## example
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x)*gy
+        return gx
+
+def add(x0,x1):
+    return Add()(x0,x1)
+
+def square(x):
+    return Square()(x) 
+
+def exp(x):
+    return Exp()(x)
+
+## example for one variable function
 #
-x = Variable(np.array(10))
-f = Square()
-y = f(x)
+# x = Variable(np.array(10))
+# f = Square()
+# y = f(x)
 # print(x.data)
-# print(y.data)        
+# print(y.data)  
+
+# example for multi variable function
+x = Variable(np.array(2.0))
+y = Variable(np.array(3.0))
+
+z = add(square(x),square(y))
+z.backward()
+
+print(z.data)
+
+print(x.grad)
+print(y.grad)      
--- Var.py
+++ Var.py
@@ -2,8 +2,7 @@ import numpy as np
 from numpy.lib.twodim_base import triu_indices_from
 from numpy.lib.utils import deprecate
 
-## Copyright 2021 Signal Processing and Machine learning at Inha Unv.
-## arthor : Gyeongmin Bae
+## Copyright 2021. Gyeonmin Bae All pictures cannot be copied without permission. 
 ## date  : 2021 - 07 - 21 (Tue)
 
 # input : instance of np.ndarray
@@ -16,20 +15,56 @@ class Variable:
         self.data = data
         self.grad = None
         self.creator = None
+        self.generation = 0
     
     #for saving creator function
     def set_creator(self, func):
         self.creator = func
+        self.generation = func.generation + 1
     
-    def backward(self):
-        f = self.creator
-        if f is not None:
-            x = f.input
-            x.grad = f.backward(self.grad)
-            x.backward()
+    def cleargrad(self):
+        self.grad = None
+    
+    def backward(self, retain_grad = False):
+        if self.grad is None : 
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key = lambda x : x.generation)
+        
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            
+            if not isinstance(gxs,tuple):
+                gxs = (gxs,)
+            
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad= gx
+                else :
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
 
 class Variable2 :
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+                
         self.data = data
         self.grad = None
         self.creator = None
@@ -61,3 +96,7 @@ x = Variable(data) # push to variable
 # right? try it yourself with your own code please 
 x.data = np.array(2.0)
 # print(x.data)
+
+x = Variable(np.array(0.5))
+x = Variable(None)
+
